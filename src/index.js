7f08ab9893e5b1ac688d9743b1ccf28f6560495b
George Rodier <grodier3@gmail.com>
2019-05-29 22:30:09 -0400

    Setup servless graphql with apollo micro

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = async (res, req) => {
-  req.end('hello world');
+const { ApolloServer, gql } = require('apollo-server-micro');
+
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => ({ username: 'George Rodier' })
+  }
 };
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
+
+module.exports = server.createHandler();
