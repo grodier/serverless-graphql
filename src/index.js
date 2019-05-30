c3e88504c6e2800638b0e64112acc2f94604208c
George.Rodier <grodier3@gmail.com>
2019-05-30 16:22:02 -0400

    Add more to options to schema

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
-const { ApolloServer, gql } = require('apollo-server-micro');
-const cors = require('micro-cors')();
+const { ApolloServer, gql } = require("apollo-server-micro");
+const cors = require("micro-cors")();
 
-const typeDefs = gql`
+const schema = gql`
   type Query {
     me: User
+    user(id: ID!): User
+    users: [User!]
   }
 
   type User {
+    id: ID!
     username: String!
   }
 `;
 
+let users = {
+  1: {
+    id: "1",
+    username: "George Rodier"
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids"
+  }
+};
+
+const me = users[1];
+
 const resolvers = {
   Query: {
-    me: () => ({ username: 'George Rodier' })
+    me: () => me,
+    user: (parent, { id }) => users[id],
+    users: () => Object.values(users)
   }
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: schema,
   resolvers,
   introspection: true,
   playground: true
