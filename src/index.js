d24568127c8a479605804e99befe7c3a0bc09911
George Rodier <grodier3@gmail.com>
2019-05-30 22:08:53 -0400

    Access context in a resolver

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const { ApolloServer, gql } = require("apollo-server-micro");
-const cors = require("micro-cors")();
+const { ApolloServer, gql } = require('apollo-server-micro');
+const cors = require('micro-cors')();
 
 const schema = gql`
   type Query {
@@ -16,20 +16,18 @@ const schema = gql`
 
 let users = {
   1: {
-    id: "1",
-    username: "George Rodier"
+    id: '1',
+    username: 'George Rodier'
   },
   2: {
-    id: "2",
-    username: "Dave Davids"
+    id: '2',
+    username: 'Dave Davids'
   }
 };
 
-const me = users[1];
-
 const resolvers = {
   Query: {
-    me: () => me,
+    me: (parent, args, { me }) => me,
     user: (parent, { id }) => users[id],
     users: () => Object.values(users)
   }
@@ -38,6 +36,7 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  context: { me: users[1] },
   introspection: true,
   playground: true
 });
