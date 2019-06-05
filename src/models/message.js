1c7f0903269065db89cad0b3b4e40fefb42ce14a
George Rodier <grodier3@gmail.com>
2019-06-04 22:05:26 -0400

    Working on db model

--- src/models/message.js
+++ src/models/message.js
@@ -1,11 +1,10 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
   text: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true }
+  user: { type: Schema.Types.ObjectId, ref: 'user', required: true }
 });
 
-const MessageModel = mongoose.model("Message", MessageSchema);
+const MessageModel = mongoose.model('Message', MessageSchema);
 
 module.exports = MessageModel;
--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,18 @@
 // started building and learning with https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
 // additional insight: https://www.robinwieruch.de/mongodb-express-setup-tutorial/
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Message = require('./message');
 
-const UserSchema = new Schema({
-  username: { type: String, required: true },
-  messages: [{ type: Schema.Types.ObjectId, ref: "Message" }]
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true }
 });
 
-const UserModel = mongoose.model("User", UserSchema);
+UserSchema.post('remove', async function(doc, next) {
+  console.log('REMOVVINNGG');
+  await Message.remove({ user: doc._id }).exec();
+  next();
+});
+
+const UserModel = mongoose.model('user', UserSchema);
 
 module.exports = UserModel;
